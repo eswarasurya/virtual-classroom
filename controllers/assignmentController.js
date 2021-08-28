aba4d32707de089c54594d8d4468f66138bef4a1
eswarasurya <eswarasurya1110@gmail.com>
2021-08-28 21:33:34 +0530

    update and delete assignments

--- controllers/assignmentController.js
+++ controllers/assignmentController.js
@@ -19,7 +19,6 @@ const createAssignment = async (req, res) => {
                 assignment.submissions.push(submission._id)
                 await submission.save()
                 await student.save()
-                
             }
         }
         const tutor = await User.findById(req.user._id)
@@ -37,6 +36,72 @@ const createAssignment = async (req, res) => {
     }
 }
 
+const removeSubmissionFromStudent = async (student, submissionId) => {
+    const index = student.submissions.indexOf(submissionId)
+    if (index === -1) return;
+    const l = student.submissions.length;
+    //swapping element to be removed to the last of array
+    [student.submissions[index], student.submissions[l - 1]] = [student.submissions[l - 1], student.submissions[index]]
+    student.submissions.pop();
+    await student.save();
+}
 
 
-module.exports = {createAssignment}
+const deleteAssignment = async (req, res) => {
+    try {
+        const assignment = await Assignment.findById(req.params.id)
+        if (!assignment) {
+            throw new Error('Invalid Assignment ID')
+        }
+        if (!assignment.createdBy.equals(req.user._id)) {
+            throw new Error('You can only delete assignments that are created by you')
+        }
+
+        const submissions = assignment.submissions
+        for (let i = 0; i < submissions.length; i++) {
+            const submission = await Submission.findById(submissions[i])
+            const student = await User.findById(submission.toStudent)
+            await removeSubmissionFromStudent(student, submissions[i]);
+            await submission.remove()
+        }
+        await assignment.remove()
+        res.status(200).json({
+            message: "Assignemnt successfully removed"
+        })
+    } catch (error) {
+        console.log(error.message)
+        res.status(401).json({
+            message: error.message
+        })
+    }
+}
+
+const updateAssignment = async (req, res) => {
+    try {
+        const {description, publishAt, deadline} = req.body;
+        const assignment = await Assignment.findById(req.params.id)
+        if (!assignment) {
+            throw new Error('Invalid Assignment ID')
+        }
+        if (!assignment.createdBy.equals(req.user._id)) {
+            throw new Error('You can only update assignments that are created by you')
+        }
+
+        if (description) assignment.description = description
+        if (publishAt) assignment.publishAt = new Date(publishAt)
+        if (deadline) assignment.deadline = new Date(deadline)
+        await assignment.save()
+        res.json({
+            message: "update Successful"
+        })
+    } catch (error) {
+        console.log(error.message)
+        res.status(400).json({
+            message: error.message
+        })
+    }
+
+}
+
+
+module.exports = {createAssignment, deleteAssignment, updateAssignment}
--- routes/assignmentRoutes.js
+++ routes/assignmentRoutes.js
@@ -4,9 +4,13 @@ const router = express.Router()
 const {protect, tutor} = require('../middleware/authMiddleware')
 
 const {
-    createAssignment
+    createAssignment,
+    deleteAssignment,
+    updateAssignment
 } = require('../controllers/assignmentController')
 
 router.post('/create', protect, tutor, createAssignment);
+router.delete('/delete/:id', protect, tutor, deleteAssignment)
+router.put('/update/:id', protect, tutor, updateAssignment);
 
 module.exports = router
